46a5d4fd653ef7ae77f3acd1d01f43ee6fe92067
KET <varlamova_e.t@mail.ru>
2022-09-09 09:41:26 +0300

    1t

---
+++ 1 t/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Second(int x)
+{
+    int a = x/10;
+    int b = a%10;
+    return $"Вторая цифра числа равна: {b}";
+}
+string result = Second(n);
+Console.WriteLine(result);
